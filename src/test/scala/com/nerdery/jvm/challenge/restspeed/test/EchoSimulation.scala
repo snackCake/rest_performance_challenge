7ce9f913b3896c534d2d2bfc5c0ebc5be92722a7
Josh Klun <jklun@nerdery.com>
2016-04-04 13:24:56 -0500

    Format & tweak Gatling script.

--- src/test/scala/com/nerdery/jvm/challenge/restspeed/test/EchoSimulation.scala
+++ src/test/scala/com/nerdery/jvm/challenge/restspeed/test/EchoSimulation.scala
@@ -1,40 +1,41 @@
 package com.nerdery.jvm.challenge.restspeed.test
 
-import scala.concurrent.duration._
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.jdbc.Predef._
+import scala.concurrent.duration._
 
 class EchoSimulation extends Simulation {
 
-	val httpProtocol = http
-		.baseURL("http://localhost:8888")
-		.inferHtmlResources()
+  private val concurrentUsers = 3600
+  private val rampSeconds = 15
+  private val baseUrl = "http://localhost:8888"
 
-	val headers_0 = Map("Upgrade-Insecure-Requests" -> "1")
+  val httpProtocol = http
+    .baseURL(baseUrl)
+    .inferHtmlResources()
+  val headers_0 = Map("Upgrade-Insecure-Requests" -> "1")
 
-    val uri1 = "http://localhost:8888/echo"
+  val scn = scenario("EchoSimulation")
+    .exec(http("request_0")
+      .get("/echo?message=%22Copy,%20Gold%20Leader%20I%27m%20already%20on%20my%20way%20out%22&name=Wedge&number=3")
+      .headers(headers_0))
+    .pause(3)
+    .exec(http("request_1")
+      .get("/echo?message=%22It%20will%20be%20like%20old%20times%20Luke%22&name=Biggs&number=3")
+      .headers(headers_0))
+    .pause(2)
+    .exec(http("request_2")
+      .get("/echo?message=%22I'm%20right%20with%20you%20Red%20three%22&name=Porkins&number=3")
+      .headers(headers_0))
+    .pause(1)
+    .exec(http("request_3")
+      .get("/echo?message=You%20need%20a%20pilot&name=Poe&number=3")
+      .headers(headers_0))
+    .pause(5)
+    .exec(http("request_4")
+      .get("/echo?message=I%20used%20to%20bullseye%20womp%20rats%20in%20my%20T-16%20back%20home%20they%27re%20not%20much%20bigger%20than%20two%20meters&name=Luke&number=1234567890")
+      .headers(headers_0))
 
-	val scn = scenario("EchoSimulation")
-		.exec(http("request_0")
-			.get("/echo?message=x&name=y&number=3")
-			.headers(headers_0))
-		.pause(1)
-		.exec(http("request_1")
-			.get("/echo?message=x&name=y&number=3")
-			.headers(headers_0))
-		.pause(2)
-		.exec(http("request_2")
-			.get("/echo?message=x&name=y&number=3")
-			.headers(headers_0)
-			.resources(http("request_3")
-			.get(uri1 + "?message=x&name=y&number=3")
-			.headers(headers_0)))
-		.pause(30)
-		.exec(http("request_4")
-			.get("/echo?message=%22This%20is%20a%20test%20of%20the%20emergency%20something%20something%22&name=jklun&number=1234567890")
-			.headers(headers_0))
-
-	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  private val userRamp = rampUsers(concurrentUsers) over (rampSeconds seconds)
+  setUp(scn.inject(userRamp)).protocols(httpProtocol)
 }
